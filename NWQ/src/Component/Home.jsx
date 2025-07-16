0aca88403be899c3a3e0513d892ef5dd8125f307
comppy <kim1122aass@gmail.com>
2025-07-16 20:28:20 +0900

    feat: 홈페이지 완료

--- NWQ/src/Component/Home.jsx
+++ NWQ/src/Component/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-
-import star from "../assets/star_icon.svg";
+import { Routes, Route, Link} from 'react-router-dom';
 import title from "../assets/Title.svg";
 import "./Home.css";
 
@@ -12,14 +11,20 @@ return (
         <img src={title} alt="신조어 퀴즈" className="main-title-svg" />
 
         {/* 시작하기 버튼 */}
-        <div className="start-button-group">
-            <button className="main-button start-button">시작하기</button>
+        <div className="start-button-group" >
+            <Link to={'/quiz'}>
+                <button className="main-button start-button">시작하기</button>
+            </Link>
         </div>
 
         {/* 설정 및 문의하기 버튼 그룹 */}
         <div className="sub-button-group">
-            <button className="main-button setting-button">설정</button>
-            <button className="main-button contact-button">문의하기</button>
+            <Link to={'/settings'}>
+                <button className="main-button setting-button" >설정</button>
+            </Link>
+            <Link to={'/requests'}>
+                <button className="main-button contact-button">문의하기</button>
+            </Link>
         </div>
         </div>
     </div>
