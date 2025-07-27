d19e593c15ecee9f51dcc2657ca4ca056c03a976
comppy <kim1122aass@gmail.com>
2025-07-27 16:30:39 +0900

    Feat: 404NotFoundPage

--- NWQ/src/App.jsx
+++ NWQ/src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import './App.css'
 import { Routes, Route, Link} from 'react-router-dom';
 import Home from './Home/Home.jsx'
+import NotFoundPage from './NotFoundPage/NotFoundPage.jsx'
 function App() {
   return (
     <>
     <Routes>
       <Route path = "/" element={<Home/>}/>
-      {/* <Route path="*" element={<NotFoundPage />}/> */}
+      <Route path="*" element={<NotFoundPage />}/>
     </Routes>
     </>
   )
