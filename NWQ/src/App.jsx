0437a47e248bf14094e16398a6ade4c280cde746
comppy <kim1122aass@gmail.com>
2025-07-30 14:25:21 +0900

    폴더정리

--- NWQ/src/App.jsx
+++ NWQ/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import { Routes, Route, Link} from 'react-router-dom';
-import Home from './Screen/Home/Home.jsx'
-import NotFoundPage from './Screen/NotFoundPage/NotFoundPage.jsx'
+import Home from './Screen/Home/index.jsx'
+import NotFoundPage from './Screen/NotFoundPage/index.jsx'
 function App() {
   return (
     <>
