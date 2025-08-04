18d9f4234329b3468eba46bcfc367487dbff131b
yeseul203 <kimyeseul153@gmail.com>
2025-08-04 16:56:32 +0900

    Decs:ScoreScreen

--- NWQ/src/scorescreen/ScoreScreenDTO.js
+++ NWQ/src/scorescreen/ScoreScreenDTO.js
@@ -1,13 +1,18 @@
 // ScoreScreenDTO.js 파일 (수정 제안)
 class ScoreScreenDTO {
-   constructor(user_id, nickname, score) { // user_id 대신 id, nickname 대신 username
-          this.user_id = user_id;
+   constructor(status, message, nickname, score) { // user_id 대신 id, nickname 대신 username
+          this.status = status;
+          this.message = message;
           this.nickname = nickname;
           this.score = score;
      }
 
-     getId() { // getUserId 대신 getId
-          return this.user_id;
+     status() { // getUserId 대신 getId
+          return this.status;
+     }
+
+     message() {
+          return this.message;
      }
 
      getUsername() { // getNickname 대신 getUsername
